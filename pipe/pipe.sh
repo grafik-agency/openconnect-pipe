592a41b2512ccfddbc52504ae924ed1f7e66bd55
Matt Miller <mmiller@grafik.com>
2023-02-16 22:02:56 -0500

    update hosts key

--- pipe/pipe.sh
+++ pipe/pipe.sh
@@ -55,6 +55,7 @@ validate() {
   : PASSWORD =${PASSWORD:?'PASSWORD variable missing.'}
   : REMOTE_PATH=${REMOTE_PATH:?'REMOTE_PATH variable missing.'}
   : LOCAL_PATH=${LOCAL_PATH:="${BITBUCKET_CLONE_DIR}/*"}
+  : PORT=${PORT:="22"}
 }
 
 vpn_connect() {
@@ -75,54 +76,57 @@ vpn_connect() {
 }
 
 setup_ssh_dir() {
-  INJECTED_SSH_CONFIG_DIR="/opt/atlassian/pipelines/agent/ssh"
-  # The default ssh key with open perms readable by alt uids
-  IDENTITY_FILE="${INJECTED_SSH_CONFIG_DIR}/id_rsa_tmp"
-  # The default known_hosts file
-  KNOWN_HOSTS_FILE="${INJECTED_SSH_CONFIG_DIR}/known_hosts"
+    INJECTED_SSH_CONFIG_DIR="/opt/atlassian/pipelines/agent/ssh"
+    # The default ssh key with open perms readable by alt uids
+    IDENTITY_FILE="${INJECTED_SSH_CONFIG_DIR}/id_rsa_tmp"
+    # The default known_hosts file
+    KNOWN_HOSTS_FILE="${INJECTED_SSH_CONFIG_DIR}/known_hosts"
 
-  mkdir -p ~/.ssh || debug "adding ssh keys to existing ~/.ssh"
-  touch ~/.ssh/authorized_keys
+    mkdir -p ~/.ssh || debug "adding ssh keys to existing ~/.ssh"
+    touch ~/.ssh/authorized_keys
 
-  if [[ -z "${PASSWORD}" ]]; then
-    # If given, use SSH_KEY, otherwise check if the default is configured and use it
-    if [ "${SSH_KEY}" != "" ]; then
-       debug "Using passed SSH_KEY"
-       (umask  077 ; echo ${SSH_KEY} | base64 -d > ~/.ssh/pipelines_id)
-    elif [ ! -f ${IDENTITY_FILE} ]; then
-       error "No default SSH key configured in Pipelines."
-       exit 1
-    else
-       debug "Using default ssh key"
-       cp ${IDENTITY_FILE} ~/.ssh/pipelines_id
+    if [[ -z "${PASSWORD}" ]]; then
+      # If given, use SSH_KEY, otherwise check if the default is configured and use it
+      if [ "${SSH_KEY}" != "" ]; then
+         debug "Using passed SSH_KEY"
+         (umask  077 ; echo ${SSH_KEY} | base64 -d > ~/.ssh/pipelines_id)
+      elif [ ! -f ${IDENTITY_FILE} ]; then
+         error "No default SSH key configured in Pipelines."
+         exit 1
+      else
+         debug "Using default ssh key"
+         cp ${IDENTITY_FILE} ~/.ssh/pipelines_id
+      fi
     fi
-  fi
 
-  if [ ! -f ${KNOWN_HOSTS_FILE} ]; then
-      error "No SSH known_hosts configured in Pipelines."
-      exit 2
-  fi
+    if [ ! -f ${KNOWN_HOSTS_FILE} ]; then
+        error "No SSH known_hosts configured in Pipelines."
+        exit 2
+    fi
+    
+    # copy know_hosts from repo to pipe
+    cat ${KNOWN_HOSTS_FILE} >> ~/.ssh/known_hosts
+    # fetch host from server and add to known_hosts
+    ssh-keyscan -p ${PORT} ${SERVER} >> ~/.ssh/known_hosts
+    if [ -f ~/.ssh/config ]; then
+        debug "Appending to existing ~/.ssh/config file"
+    fi
 
-  cat ${KNOWN_HOSTS_FILE} >> ~/.ssh/known_hosts
-  if [ -f ~/.ssh/config ]; then
-      debug "Appending to existing ~/.ssh/config file"
-  fi
-
-  if [[ -z "${PASSWORD}" ]]; then
-    echo "IdentityFile ~/.ssh/pipelines_id" >> ~/.ssh/config
-  fi
-  chmod -R go-rwx ~/.ssh/
+    if [[ -z "${PASSWORD}" ]]; then
+      echo "IdentityFile ~/.ssh/pipelines_id" >> ~/.ssh/config
+    fi
+    chmod -R go-rwx ~/.ssh/
 }
 
 run_pipe() {
     info "Starting SFTP deployment to ${SERVER}:${REMOTE_PATH}..."
     set +e
     if [[ -z "${PASSWORD}" ]]; then
-      debug Executing echo \"mput ${LOCAL_PATH}\" \| sftp -b - -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} ${USER}@${SERVER}:${REMOTE_PATH}
-      echo "mput ${LOCAL_PATH}" | sftp -b - -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} ${USER}@${SERVER}:${REMOTE_PATH}
+      debug Executing echo \"mput ${LOCAL_PATH}\" \| sftp -b - -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} -P ${PORT} ${USER}@${SERVER}:${REMOTE_PATH}
+      echo "mput ${LOCAL_PATH}" | sftp -b - -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} -P ${PORT} ${USER}@${SERVER}:${REMOTE_PATH}
     else
-      debug Executing echo \"mput ${LOCAL_PATH}\" \| sshpass -p ${PASSWORD} sftp -o PubkeyAuthentication=no -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} ${USER}@${SERVER}:${REMOTE_PATH}
-      echo "mput ${LOCAL_PATH}" | sshpass -p ${PASSWORD} sftp -o PubkeyAuthentication=no -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} ${USER}@${SERVER}:${REMOTE_PATH}
+      debug Executing echo \"mput ${LOCAL_PATH}\" \| sshpass -p ${PASSWORD} sftp -o PubkeyAuthentication=no -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} -P ${PORT} ${USER}@${SERVER}:${REMOTE_PATH}
+      echo "mput ${LOCAL_PATH}" | sshpass -p ${PASSWORD} sftp -o PubkeyAuthentication=no -rp ${SFTP_DEBUG_ARGS} ${EXTRA_ARGS} -P ${PORT} ${USER}@${SERVER}:${REMOTE_PATH}
     fi
     STATUS=$? # status of last command of pipe, i.e. sftp
     set -e
